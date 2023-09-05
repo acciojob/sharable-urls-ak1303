07df0e1d24773f669b33c5bf4939c8e17f5d295a
ak1303 <94529241+ak1303@users.noreply.github.com>
2023-09-05 18:00:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
-document.getElementById('myForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    // Get user input values
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
     var name = document.getElementById('name').value;
     var year = document.getElementById('year').value;
-
-    // Build the query string
-    var queryString = '';
-
-    if (name) {
-        queryString += 'name=' + encodeURIComponent(name);
-    }
-
-    if (year) {
-        if (queryString) {
-            queryString += '&';
-        }
-        queryString += 'year=' + encodeURIComponent(year);
-    }
-
-    // Update the URL in the h3 element
-    document.getElementById('url').textContent = 'https://localhost:8080/' + (queryString ? '?' + queryString : '');
+    var url = 'https://localhost:8080/?name=' + name + '&year=' + year;
+    document.getElementById('url').textContent = url;
 });
-
-
