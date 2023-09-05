79a2dfbd88d9d4168755b3f1186a58f3fa56c6c3
ak1303 <94529241+ak1303@users.noreply.github.com>
2023-09-05 17:50:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-// your code here
 document.getElementById('myForm').addEventListener('submit', function (e) {
-            e.preventDefault();
-            
-            // Get user input values
-            var name = document.getElementById('name').value;
-            var year = document.getElementById('year').value;
+    e.preventDefault();
 
-            // Build the query string
-            var queryString = 'name=' + encodeURIComponent(name) + '&year=' + encodeURIComponent(year);
+    // Get user input values
+    var name = document.getElementById('name').value;
+    var year = document.getElementById('year').value;
 
-            // Update the URL in the h3 element
-            document.getElementById('url').textContent = 'https://localhost:8080/?' + queryString;
-        });
+    // Build the query string
+    var queryString = '';
+
+    if (name) {
+        queryString += 'name=' + encodeURIComponent(name);
+    }
+
+    if (year) {
+        if (queryString) {
+            queryString += '&';
+        }
+        queryString += 'year=' + encodeURIComponent(year);
+    }
+
+    // Update the URL in the h3 element
+    document.getElementById('url').textContent = 'https://localhost:8080/' + (queryString ? '?' + queryString : '');
+});
+
+
